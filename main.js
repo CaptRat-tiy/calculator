b67ec024c01d3fda3080e5ecdf54ddb722a7f2f4
CaptRat <jpatricklogan@gmail.com>
2017-03-14 19:24:52 -0600

    feat (.js) Add all getElementbyId button text; finishing trouble-shooting

--- main.js
+++ main.js
@@ -1,65 +1,153 @@
 //SIMPLE FUNCTIONS
 
-function addNum (num1, num2) {
-  var sum = num1 + num2;
-  sum = Math.floor(sum*10000000000)/10000000000;
-  console.log(sum);
-  return sum;
-}
-console.assert(addNum(7,2) === 9);
+// function addNum (num1, num2) {
+//   var sum = num1 + num2;
+//   sum = Math.floor(sum*10000000000)/10000000000;
+//   console.log(sum);
+//   return sum;
+// }
+// console.assert(addNum(7,2) === 9);
+//
+//
+// function subtNum (num1, num2) {
+//   var answer = parseFloat(num1 - num2);
+//   answer = Math.floor(answer*10000000000)/10000000000;
+//   console.log(answer);
+//   return answer;
+// }
+// console.assert(subtNum(8,3) === 5);
+// console.assert(subtNum(90, -35) === 125);
+//
+//
+// function multNum (num1, num2) {
+//   let product = parseFloat(num1 * num2);
+//   product = Math.floor(product*10000000000)/10000000000;
+//   console.log(product);
+//   return product;
+// }
+// console.assert(multNum(22,7)===154);
+// console.assert(multNum(7, 3.2)===22.4);
+//
+//
+// function divNum (num1, num2) {
+//   if (num2 ===0) {return "illegal divide by zero request"};
+//   let quotient = parseFloat(num1 / num2);
+//   quotient = Math.floor(quotient *10000000000)/10000000000;
+//   console.log(quotient);
+//   return quotient;
+// }
+// console.assert (divNum(31, 11)===2.8181818181);
+//
+// function modulus (num1, num2) {
+//   let result = parseFloat(num1 % num2)
+//   result = Math.floor(result *10000000000)/10000000000;
+//   console.log(result)
+// return result;
+// }
+// console.assert (modulus (7, 3)===1);
+// console.assert (modulus (8, 2)===0);
 
 
-function subtNum (num1, num2) {
-  var answer = parseFloat(num1 - num2);
-  answer = Math.floor(answer*10000000000)/10000000000;
-  console.log(answer);
-  return answer;
-}
-console.assert(subtNum(8,3) === 5);
-console.assert(subtNum(90, -35) === 125);
 
 
-function multNum (num1, num2) {
-  let product = parseFloat(num1 * num2);
-  product = Math.floor(product*10000000000)/10000000000;
-  console.log(product);
-  return product;
-}
-console.assert(multNum(22,7)===154);
-console.assert(multNum(7, 3.2)===22.4);
+var buttonOne = document.getElementById("buttonOne");
+console.log(buttonOne)
+
+buttonOne.addEventListener("click", function () {
+  console.log("hi");
+});
 
 
-function divNum (num1, num2) {
-  if (num2 ===0) {return "illegal divide by zero request"};
-  let quotient = parseFloat(num1 / num2);
-  quotient = Math.floor(quotient *10000000000)/10000000000;
-  console.log(quotient);
-  return quotient;
-}
-console.assert (divNum(31, 11)===2.8181818181);
+var buttonTwo = document.getElementById("buttonTwo");
 
-function modulus (num1, num2) {
-  let result = parseFloat(num1 % num2)
-  result = Math.floor(result *10000000000)/10000000000;
-  console.log(result)
-return result;
-}
-console.assert (modulus (7, 3)===1);
-console.assert (modulus (8, 2)===0);
+buttonTwo.addEventListener("click", function () {
+  console.log("hi");
+});
+
+var buttonThree = document.getElementById("buttonThree");
+
+buttonThree.addEventListener("click", function () {
+  console.log("hi");
+});
 
 
+var buttonFour = document.getElementById("buttonFour");
+
+buttonFour.addEventListener("click", function () {
+  console.log("hi");
+});
 
 
+var buttonFive = document.getElementById("buttonFive");
 
-var oneNode = document.querySelector("p#1")
-var twoNode = document.querySelector("p#2")
-var oneNode = document.querySelector("p#1")
-var oneNode = document.querySelector("p#1")
-var oneNode = document.querySelector("p#1")
-var oneNode = document.querySelector("p#1")
-var oneNode = document.querySelector("p#1")
-var oneNode = document.querySelector("p#1")
-var oneNode = document.querySelector("p#1")
-var oneNode = document.querySelector("p#1")
-var oneNode = document.querySelector("p#1")
-var oneNode = document.querySelector("p#1")
+buttonFive.addEventListener("click", function () {
+  console.log("hi");
+});
+
+var buttonSix = document.getElementById("buttonSix");
+
+buttonSix.addEventListener("click", function () {
+  console.log("hi");
+});
+
+
+var buttonSeven = document.getElementById("buttonSeven");
+
+buttonSeven.addEventListener("click", function () {
+  console.log("hi");
+});
+
+
+var buttonEight = document.getElementById("buttonEight");
+
+buttonEight.addEventListener("click", function () {
+  console.log("hi");
+});
+
+
+var buttonNine = document.getElementById("buttonNine");
+
+
+buttonNine.addEventListener("click", function () {
+  console.log("hi");
+});
+
+
+var buttonZero = document.getElementById("buttonZero");
+
+buttonZero.addEventListener("click", function () {
+  console.log("hi");
+});
+
+
+var buttonMult = document.getElementById("buttonMult");
+
+buttonMult.addEventListener("click", function () {
+  console.log("hi");
+});
+
+
+var buttonDivide = document.getElementById("buttonDivide");
+
+buttonDivide.addEventListener("click", function () {
+  console.log("hi");
+});
+
+
+var buttonAdd = document.getElementById("buttonAdd");
+
+buttonAdd.addEventListener("click", function () {
+  console.log("hi");
+});
+
+var buttonSubtract = document.getElementById("buttonSubtract");
+
+buttonSubtract.addEventListener("click", function () {
+  console.log("hi");
+});
+
+var buttonEquals = document.getElementById("buttonEquals");
+
+buttonEquals.addEventListener("click", function () {
+  console.log("hi");
+});

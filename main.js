3dd5c8335bcecb9b62b51753cb200b185547d65b
captrat <jpatricklogan@gmail.comgit config user.name>
2017-04-03 16:11:10 -0600

    feat (console) Add buttonNodeList in Console

--- main.js
+++ main.js
@@ -7,12 +7,11 @@ function onLoad () {
   var equalButton = document.getElementById("buttonEquals");
   equalButton.addEventListener("click", function () {
     let answer = calculate(input[0], input[2], input[1]);
-    console.log(answer);
+    // console.log(answer);
     document.getElementById('output').innerHTML = answer;
   }
 )
-  // console.log(buttonNodeList)
-
+  console.log(buttonNodeList)
 /* and this displays every button's into the display*/
   for (let item of buttonNodeList) {
     // console.log(item.value);
@@ -29,8 +28,8 @@ function onLoad () {
 window.onload = onLoad;
 
 function calculate(num1, num2, operator) {
-  num1 = parseInt(num1);
-  num2 = parseInt(num2);
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
   if (operator === "+") {
     return addNum(num1, num2)
   }
@@ -77,112 +76,7 @@ function divNum (num1, num2) {
 function modulus (num1, num2) {
   let result = parseFloat(num1 % num2)
   result = Math.floor(result *10000000000)/10000000000;
-  // console.log(result)
 return result;
 }
 
 console.log('script loaded');
-//
-//
-// var buttonOne = document.getElementById("buttonOne");
-//
-// buttonOne.addEventListener("click", function () {
-//     console.log(item.value)
-// });
-//
-
-// var buttonTwo = document.getElementById("buttonTwo");
-//
-// buttonTwo.addEventListener("click", function () {
-// console.log(buttonTwo.value)})
-// var buttonThree = document.getElementById("buttonThree");
-//
-// buttonThree.addEventListener("click", function () {
-//   console.log("this is 3");
-// });
-//
-//
-// var buttonFour = document.getElementById("buttonFour");
-//
-// buttonFour.addEventListener("click", function () {
-//   console.log("this is 4");
-// });
-//
-//
-// var buttonFive = document.getElementById("buttonFive");
-//
-// buttonFive.addEventListener("click", function () {
-//   console.log("this is 5");
-// });
-//
-// var buttonSix = document.getElementById("buttonSix");
-//
-// buttonSix.addEventListener("click", function () {
-//   console.log("this is 6");
-// });
-//
-//
-// var buttonSeven = document.getElementById("buttonSeven");
-//
-// buttonSeven.addEventListener("click", function () {
-//   console.log("this is 7");
-// });
-//
-//
-// var buttonEight = document.getElementById("buttonEight");
-//
-// buttonEight.addEventListener("click", function () {
-//   console.log("this is 8");
-// });
-//
-//
-// var buttonNine = document.getElementById("buttonNine");
-//
-// buttonNine.addEventListener("click", function () {
-//   console.log(parseInt(this.textContent))
-// });
-//
-//
-// var buttonZero = document.getElementById("buttonZero");
-//
-// buttonZero.addEventListener("click", function () {
-//   console.log("this is 0");
-// });
-
-//
-//
-// var buttonMult = document.getElementById("buttonMult");
-//
-// buttonMult.addEventListener("click", function () {
-//   console.log("this is Multipy");
-// });
-//
-//
-// var buttonDivide = document.getElementById("buttonDivide");
-//
-// buttonDivide.addEventListener("click", function () {
-//   console.log("this is Divide");
-// });
-//
-//
-// var buttonAdd = document.getElementById("buttonAdd");
-//
-// buttonAdd.addEventListener("click", function () {
-//   console.log("this is Add");
-// });
-//
-// var buttonSubtract = document.getElementById("buttonSubtract");
-//
-// buttonSubtract.addEventListener("click", function () {
-//   console.log("this is Subtract");
-// });
-//
-// var buttonEquals = document.getElementById("buttonEquals");
-//
-// buttonEquals.addEventListener("click", function () {
-//   console.log(parseInt(this.value));
-// });
-
-
-
-//This 'output'.innerHTML doesn't load any value, just placeholder
